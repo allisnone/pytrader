6673087a1c556e07213f53bfc61a6c0ae2f02cba
allisnone <zgx20022002@163.com>
2019-03-30 14:47:52 +0800

    upload

---
+++ mail/enc.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#__Author__= allisnone #2019-02-16
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+import base64
+
+def aes_cbc_encrypt(message, key):
+    '''
+    use AES CBC to encrypt message, using key and init vector
+    :param message: the message to encrypt
+    :param key: the secret  key = '1234567890!@#$%^' 
+    :return: bytes init_vector + encrypted_content
+    '''
+    iv_len = 16
+    assert type(message) in (str,bytes)
+    assert type(key) in (str,bytes)
+    if type(message) == str:
+        message = bytes(message, 'utf-8')
+    if type(key) == str:
+        key = bytes(key, 'utf-8')
+    backend = default_backend()
+    iv = os.urandom(iv_len)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message) + padder.finalize()
+    enc_content = encryptor.update(padded_data) + encryptor.finalize()
+    return iv + enc_content
+
+def aes_cbc_decrypt(content, key=b'MTIzNDU2Nzg5MCFAIyQlXg=='):
+    '''
+    use AES CBC to decrypt message, using key
+    :param content: the encrypted content using the above protocol
+    :param key: the secret
+    :return: decrypted bytes
+    '''
+    assert type(content) == bytes
+    key = base64.decodestring(key).decode('utf-8')
+    assert type(key) in (bytes, str)
+    if type(key) == str:
+        key = bytes(key, 'utf-8')
+    iv_len = 16
+    assert len(content) >= (iv_len + 16)
+    iv = content[:iv_len]
+    enc_content = content[iv_len:]
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    unpadder = padding.PKCS7(128).unpadder()
+    decryptor = cipher.decryptor()
+    dec_content = decryptor.update(enc_content) + decryptor.finalize()
+    real_content = unpadder.update(dec_content) + unpadder.finalize()
+    return real_content
+
+#from app.libs.crypto_sign import *
+def get_random_key_readable(key_size=256):
+    '''
+    get random key for symmetric encryption
+    with key_size bits
+    :param key_size: bit length of the key
+    :return: bytes key
+    '''
+    # length for urandom
+    ulen = int(key_size/8/4*3)
+    key = base64.b64encode(os.urandom(ulen))
+    return key
+
+#bs = base64.b64encode(bytes('abcde1@163.com','ascii'))
+#print(bs)
+#base64.decodestring(bs).decode('utf-8')
+                 
---
+++ mail/mylogger.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#__Author__= allisnone #2019-02-16
+#https://www.cnblogs.com/yyds/p/6901864.html
+import logging
+import logging.handlers
+import datetime
+import base64
+from enc import aes_cbc_decrypt
+
+class Logger(object):
+    def __init__(self,logfile='all.log',errorfile='error.log',logname='mylogger',level=logging.DEBUG,tolist=['104450966@qq.com']):
+        self.logger = logging.getLogger(logname)
+        self.logger.setLevel(level)
+        rf_handler = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
+        rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+        f_handler = logging.FileHandler(errorfile)
+        f_handler.setLevel(logging.ERROR)
+        f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+        self.m_handler = None
+        self.logger.addHandler(rf_handler)
+        self.logger.addHandler(f_handler)
+        #logger.addHandler(m_handler)
+        self.tomails = tolist
+        
+    def set_tomails(self,tolist): 
+        self.tomails = tolist
+        return
+    
+    def set_mail_handle(self,subject,handle=None):
+        rand = b'^\xe3\xb1*PxXR\xcb\x92\tc\xa2\x84\xf3W\xab\xca\xf9\x88\xb1)\x9b\xc7\xd4p\xb8\xf6b\xc7\xd7\x91'  #8Z
+        gx = b'emd4MjAwMjIwMDJAMTYzLmNvbQ=='
+        em = base64.decodestring(gx).decode('utf-8')
+        if handle:
+            self.m_handler = handle
+        else:
+            self.m_handler = logging.handlers.SMTPHandler(('smtp.163.com',25),
+                                                 fromaddr= em,
+                                                 toaddrs=self.tomails,
+                                                 subject = subject,
+                                                 credentials = (em,aes_cbc_decrypt(rand).decode('utf-8'))
+                                                 )  
+        
+    def send_mail(self,subject = 'Just for logger test',handle=None):
+        self.logger.info('Send mail message to {}'.format(self.tomails))
+        self.set_mail_handle(subject,handle=None)
+        self.logger.addHandler(self.m_handler)
+        return
+    
+    def stop_send_mail(self):
+        self.logger.removeHandler(self.m_handler)
+        self.logger.info('Cancel send mail message to {}'.format(self.tomails))
+        return
+"""    
+LOG = Logger(logfile='all.log',errorfile='error.log',logname='mylogger',level=logging.DEBUG,tolist=['104450966@qq.com'])
+LOG.logger.debug('debug message')
+LOG.logger.info('info message')
+LOG.logger.warning('warning message')
+LOG.send_mail(subject='add error message mail')
+LOG.logger.error('error message')
+LOG.stop_send_mail()
+LOG.logger.critical('critical message')
+LOG.logger.critical('critical message1')
+"""
