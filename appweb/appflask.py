4e0312043308490c068a17daae7b9bf225a292ad
allisnone <zgx20022002@163.com>
2019-03-31 18:59:45 +0800

    update

--- appweb/appflask.py
+++ appweb/appflask.py
@@ -10,3 +10,5 @@ db_path = os.path.dirname(os.path.abspath(__file__))
 db_path = db_path.replace('appweb','trademodel')
 apps.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(db_path,'pytrader.db')
 dbs = SQLAlchemy(apps)
+#SQLALCHEMY_TRACK_MODIFICATIONS = True #C:\Python3.5\Lib\site-packages\flask_sqlalchemy
+#SQLALCHEMY_COMMIT_TEARDOWN = True
